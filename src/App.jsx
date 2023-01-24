c5a9795bf4061060685c26350f611c911b7b81c6
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-01-23 20:39:28 -0500

    header, budget component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <h1>hi</h1>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
