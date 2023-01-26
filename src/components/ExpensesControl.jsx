f74d9245122299eaed9dae00dfa3957e701f9f7e
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-01-26 11:23:14 -0500

    Add currency format

--- src/components/ExpensesControl.jsx
+++ src/components/ExpensesControl.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
 const ExpensesControl = ({budget}) => {
+    const formatBudget = (amount) => {
+        return amount.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        })
+    }
   return (
     <div className='budget-container container shadow two-columns'>
         <div>
             grafica
         </div>
         <div className='budget-content'>
-            <span>Budget: </span> ${budget}
+            <p><span>Budget: </span>{formatBudget(budget)}</p>
+            <p><span>Available: </span>{formatBudget(0)}</p>
+            <p><span>Balance: </span>{formatBudget(0)}</p>
         </div>
     </div>
   )
