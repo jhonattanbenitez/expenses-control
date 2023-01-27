aab3a6a5a22c6f2bf6c891eb92c8053a65f0b8bc
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-01-27 11:48:25 -0500

    add modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
 import { useState } from 'react';
+import NewExpenseIcon from './img/newExpense.svg';
 import Header from './components/Header';
+import Modal from './components/Modal';
 
 function App() {
   const [budget, setBudget] = useState(0);
-  const [isValidBudget, setIsValidBudget] = useState(0)
+  const [isValidBudget, setIsValidBudget] = useState(0);
+  const [modal, setModal] = useState(false);
+  const [animateModal, setAnimateModal] = useState(false);
+
+  const handleNewExpense = () => {
+    setModal(true);
+    setTimeout(() => {
+       setAnimateModal(true)
+    }, 1500)
+  };
 
   return (
     <>
-      <Header 
+      <Header
         budget={budget}
         setBudget={setBudget}
         isValidBudget={isValidBudget}
         setIsValidBudget={setIsValidBudget}
       />
+      {isValidBudget && (
+        <div className="new-expense">
+          <img
+            src={NewExpenseIcon}
+            alt="new expense icon"
+            onClick={handleNewExpense}
+          />
+        </div>
+      )}
+
+      {modal && <Modal setModal={setModal} animateModal={animateModal}/>}
     </>
   );
 }
