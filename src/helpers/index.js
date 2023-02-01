70e1f242af5c5c38e3f013fe164720ef68862f60
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-02-01 13:52:53 -0500

    add expense component with images

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,17 @@
 export const idGenerator = () => {
-    const random = Math.random().toString(36).substring(2)
-    const date = Date.now().toString(36)
+  const random = Math.random().toString(36).substring(2);
+  const date = Date.now().toString(36);
 
-    return random + date;
-}
+  return random + date;
+};
+
+export const dateFormatter = (date) => {
+  const newDate = new Date(date);
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+  };
+
+  return newDate.toLocaleDateString('en-US', options);
+};
